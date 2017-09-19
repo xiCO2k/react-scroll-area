aa14bb237464b616e5d138d425aec3c1abc86586
Francisco Madeira <xico2k@gmail.com>
2017-09-19 18:38:39 +0100

    documentation

--- webpack.config.docs.js
+++ webpack.config.docs.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: resolve(__dirname, 'docs'),
-        publicPath: '/'
+        publicPath: ''
     },
     module: {
         rules: [{
